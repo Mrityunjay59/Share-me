3bc08e020a66996a6e948f409ffe081aa5608947
mrityunjay raj <mrityunjayyadav59@gmail.com>
2023-09-01 23:43:48 +0530

    base url fix

--- shareme-FE/index.js
+++ shareme-FE/index.js
@@ -16,7 +16,7 @@ const emailForm = document.querySelector("#emailForm");
 const toast = document.querySelector(".toast");
 
 // const baseURL = "https://file-sharing-be.onrender.com";
-const baseURL = "http://localhost:3000";
+const baseURL = "https://file-sharing-be.onrender.com";
 
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
